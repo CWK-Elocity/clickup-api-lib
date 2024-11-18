7f66549fc72e4fe60b306a326daf008afa78e52c
RyKaT07 <patrykski07@gmail.com>
2024-11-18 13:43:54 +0100

    fix: updated url

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="clickup-api-lib",  
+    version="0.1.0",  
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.0.0",
+    ],
+    description="Library for interacting with the ClickUp API",
+    author="Patryk Skibniewski",
+    author_email="patrykski07@gmail.com",
+    url="https://github.com/RyKaT07/clickup-api-lib.git",
+    python_requires=">=3.7"
+)
